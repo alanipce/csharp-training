eb990848be1fff45a33e101e2b8c9e6edb6d790b
Alan Perez <alanipce@gmail.com>
2018-10-09 11:24:26 -0700

    Added new algorithms project and implemented mergesort
    **Lesson learned be careful when computing the mid and assigning it to the left and right partition. I ended up with a Stack Overflow situation caused by unterminated recursion.
    
    Situation
    array: [25, 11]
    start: 0
    end: 1
    
    mid = start + (end - start) / 2
    mid = 0
    
    left: []
    right: [25, 11]
    
    I was assigning the mid to right partion meaning left partition had zero elements and right partition had both the original elements, this would continue until the stack overflowed. I could have done one of two things update the mid formula to add one and continue assigning to right, or keep mid formula the same and assign to left. I went with the latter.
    
    Evenly sized arrays (greater than one element) dont have a single mid element, can think of it having two mid elements a left mid and right mid. My problem was I was assigning the left mid to the right partition.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 10, 25, 7, 2, 0 };
+
+            Console.WriteLine("Before sort: {0}", string.Join(',', list));
+            mergesort(list);
+            Console.WriteLine("After sort: {0}", string.Join(',', list));
+        }
+
+        static void mergesort(int[] array) {
+            mergesort(array, 0, array.Length - 1);
+        }
+
+        static void mergesort(int[] array, int start, int end) 
+        {
+            if (start >= end || start < 0 || end >= array.Length) 
+            {
+                return;
+            }
+
+            int mid = start + (end - start) / 2;
+            mergesort(array, start, mid);
+            mergesort(array, mid + 1, end);
+
+            merge(array, start, mid, end);
+        }
+
+        static void merge(int[] array, int start, int mid, int end) 
+        {
+            // copy the contents of the array's left and right partition to merge w/o overwriting
+            int[] leftArray = new int[mid - start + 1];
+            int[] rightArray = new int[end - mid];
+
+            for (int i = 0; i < leftArray.Length; ++i) {
+                leftArray[i] = array[start + i];
+            }
+
+            for (int i = 0; i < rightArray.Length; ++i) {
+                rightArray[i] = array[mid + 1 + i];
+            }
+
+            int leftPtr = 0;
+            int rightPtr = 0;
+            int arrayPtr = start;
+
+            while (leftPtr < leftArray.Length && rightPtr < rightArray.Length) {
+                if (leftArray[leftPtr] <= rightArray[rightPtr]) {
+                    array[arrayPtr] = leftArray[leftPtr];
+                    ++leftPtr;
+                    ++arrayPtr;
+                } else {
+                    array[arrayPtr] = rightArray[rightPtr];
+                    ++rightPtr;
+                    ++arrayPtr;
+                }
+            }
+
+
+            // copy the remaining items of the remaining partition
+            while (leftPtr < leftArray.Length) {
+                array[arrayPtr] = leftArray[leftPtr];
+                ++leftPtr;
+                ++arrayPtr;
+            }
+
+            while (rightPtr < rightArray.Length) {
+                array[arrayPtr] = rightArray[rightPtr];
+                ++rightPtr;
+                ++arrayPtr;
+            }
+        }
+
+    }
+}
